efff0f8f5170bf2a2b236b19d127cfeec1dec7d6
Mohit Patel <76245882+MohittPattel@users.noreply.github.com>
2021-04-19 22:34:31 +0530

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>I'm Mohit.</h2>
+        <p>I'm from Jabalpur Madhya Pradesh. I'm a under
+          graduate student of Shri Ram Institute of Technology (Jabalpur)
+          specelized in Information Technology. I complete my schooling from S.T
+          Mary Higher Secondary School (Jabalpur). Awarded by ' Chief Minister
+          Maritious Student Award 2018' by the Chief Minister of Madhya Pradesh
+          " Shri Shivraj Singh Chouhan " for performing good in the Higher
+          Secondary Exam.
+        </p>
+                
+        <ol>
+                    <li>Nulla pulvinar diam</li>
+                    <li>Facilisis bibendum</li>
+                    <li>Vestibulum vulputate</li>
+                    <li>Eget erat</li>
+                    <li>Id porttitor</li>
+                  
+        </ol>
+              
+      </div>
+    );
+  }
+}
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Email</h2>
+        <a href=".http//www.gmail.com">forums</a>.
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
